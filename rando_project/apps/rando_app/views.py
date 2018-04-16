f2667b54567ed2ae2747e6c47f061eade0bf53ef
RC432 <rclewis432@gmail.com>
2018-04-16 15:08:13 -0700

    first draft rando

---
+++ rando_project/apps/rando_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
+
+def index(request):
+    if 'counter' not in request.session:
+        request.session['counter'] = 0
+    request.session['counter'] += 1
+    request.session['random_word'] = get_random_string(length = 14)
+
+    return render(request, 'rando_app/index.html')
+
+def reset(request):
+    print 'this route works!'
+    request.session['counter'] = 0
+    # return redirect(request, '/random_word/reset') doesn't work!!
+    return render(request, 'rando_app/index.html')
